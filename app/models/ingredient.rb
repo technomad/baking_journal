0f42a1035019026f87f19197a413ca48e3a2a57f
technomad <technomad@gmail.com>
2013-03-28 18:35:37 -0400

    Trying find_or_create

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,9 @@ class Ingredient < ActiveRecord::Base
   has_many :entry_ingredients
   has_many :entries, :through => :entry_ingredients
   
+  def ingredient_name
+    @name = self.name
+    @ingredient = self.find_or_create_by_name(params[:name])
+  end
+  
 end
